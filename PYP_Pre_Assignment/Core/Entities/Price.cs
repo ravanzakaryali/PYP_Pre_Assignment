391d05056fee38c40fc72068f2328ab5fc4f6154
revanzli <revanzekeryeli2002@gmail.com>
2022-08-18 09:14:53 +0400

    feat: added Price entity

---
+++ PYP_Pre_Assignment/Core/Entities/Price.cs
@@ -0,0 +1,13 @@
+﻿using Core.Entities.Base;
+
+namespace Core.Entities
+{
+    public class Price : BaseEntity<Guid>
+    {
+        public decimal SalePrice { get; set; }
+        public decimal ManufacturingPrice { get; set; }
+        public Guid ProductId { get; set; }
+        public Product Product { get; set; }
+
+    }
+}
