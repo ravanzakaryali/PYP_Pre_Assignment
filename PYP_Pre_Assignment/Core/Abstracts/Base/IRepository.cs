8a815506b60d91d25558b6a437daf95f443f09f2
revanzli <revanzekeryeli2002@gmail.com>
2022-08-18 09:41:02 +0400

    feat: added repository abstract

---
+++ PYP_Pre_Assignment/Core/Abstracts/Base/IRepository.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq.Expressions;
+
+namespace Core.Abstracts.Base
+{
+    public interface IRepository<TEntity>
+    {
+        Task<List<TEntity>> GetAllAsync(
+            Expression<Func<TEntity, bool>> predicate = null,
+            bool tracking = true,
+            params string[] includes);
+        Task<List<TResult>> GetAllWithSelectAsync<TResult>(
+            Expression<Func<TEntity, TResult>> select,
+            Expression<Func<TEntity, bool>> predicate = null,
+            bool tracking = true,
+            params string[] includes);
+        Task<List<TEntity>> GetAllAsync<TOrderBy>(
+            int page,
+            int size,
+            Expression<Func<TEntity, TOrderBy>> orderBy,
+            Expression<Func<TEntity, bool>> predicate = null,
+            bool isOrderBy = true,
+            bool tracking = true,
+            params string[] includes);
+        Task<TEntity> GetAsync(
+            Expression<Func<TEntity, bool>> predicate = null,
+            bool tracking = true,
+            params string[] include);
+        Task<TResult> GetWithSelectAsync<TResult>(
+            Expression<Func<TEntity, TResult>> select,
+            Expression<Func<TEntity, bool>> where = null,
+            bool tracking = true,
+            params string[] includes);
+        Task<List<TResult>> GetAllWithSelectAsync<TOrderBy, TResult>(
+            int page,
+            int size,
+            Expression<Func<TEntity, TOrderBy>> orderBy,
+            Expression<Func<TEntity, TResult>> select,
+            Expression<Func<TEntity, bool>> predicate = null,
+            bool isOrderBy = true,
+            bool tracking = true,
+            params string[] includes);
+        Task<TEntity> AddAsync(TEntity entity);
+        TEntity Remove(TEntity entity);
+        TEntity Update(TEntity entity);
+        Task<int> SaveAsync();
+    }
+}
