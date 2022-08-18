eb476f2b247ffa1c1d6fbab226b063f31c54339b
revanzli <revanzekeryeli2002@gmail.com>
2022-08-18 09:16:51 +0400

    feat: added SaleTransaction entity

---
+++ PYP_Pre_Assignment/Core/Entities/SaleTransaction.cs
@@ -0,0 +1,21 @@
+﻿using Core.Entities.Base;
+
+namespace Core.Entities
+{
+    public class SaleTransaction : BaseEntity<Guid>
+    {
+        public decimal UnitSold { get; set; }
+        public decimal Discounts { get; set; }
+        public decimal Sales { get; set; }
+        public DateTime Date { get; set; }
+        public Guid PriceId { get; set; }
+        public Price Price { get; set; }
+        public Guid ProductId { get; set; }
+        public Product Product { get; set; }
+        public Guid SegmentId { get; set; }
+        public Segment Segment { get; set; }
+        public Guid CountryId { get; set; }
+        public Country Country { get; set; }
+
+    }
+}
